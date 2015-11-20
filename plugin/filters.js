f61b972a24b54581a806eced33b272381a20185b
Nicolas Siver <nicolas@siver.im>
2015-11-19 19:25:34 -0500

    filter settings

--- plugin/filters.js
+++ plugin/filters.js
@@ -1,8 +1,19 @@
 (function (Filters) {
     'use strict';
 
+    var nodebb = require('./nodebb');
+
+    var meta = nodebb.meta;
+
     Filters.getConfig = function (config, callback) {
-        return config;
+        // Use always traditional pagination
+        config.usePagination = true;
+
+        // Prevent override by user's settings
+        config.topicsPerPage = meta.config.topicsPerPage || 20;
+        config.postsPerPage = meta.config.postsPerPage || 20;
+
+        callback(null, config);
     };
 
 })(module.exports);
--- plugin/index.js
+++ plugin/index.js
@@ -4,6 +4,8 @@
 (function (Plugin) {
     'use strict';
 
+    var filters = require('./filters');
+
     //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
     Plugin.hooks = {
         filters: filters
